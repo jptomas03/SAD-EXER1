cdf980df8020d3671425afec64d91681598b889e
jptomas03 <jptomas03@yahoo.com>
2015-08-18 10:48:29 +0800

    exer1
    first commit

---
+++ SAD-EXER1/Calendar.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Calendar
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lblDay = New System.Windows.Forms.Label()
+        Me.lblTime = New System.Windows.Forms.Label()
+        Me.lblMonth = New System.Windows.Forms.Label()
+        Me.lblNumber = New System.Windows.Forms.Label()
+        Me.lblYear = New System.Windows.Forms.Label()
+        Me.timDisplay = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'lblDay
+        '
+        Me.lblDay.AutoSize = True
+        Me.lblDay.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDay.Location = New System.Drawing.Point(12, 24)
+        Me.lblDay.Name = "lblDay"
+        Me.lblDay.Size = New System.Drawing.Size(140, 39)
+        Me.lblDay.TabIndex = 0
+        Me.lblDay.Text = "Monday"
+        '
+        'lblTime
+        '
+        Me.lblTime.AutoSize = True
+        Me.lblTime.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTime.Location = New System.Drawing.Point(12, 85)
+        Me.lblTime.Name = "lblTime"
+        Me.lblTime.Size = New System.Drawing.Size(212, 39)
+        Me.lblTime.TabIndex = 1
+        Me.lblTime.Text = "00:00:00 PM"
+        '
+        'lblMonth
+        '
+        Me.lblMonth.AutoSize = True
+        Me.lblMonth.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMonth.Location = New System.Drawing.Point(252, 24)
+        Me.lblMonth.Name = "lblMonth"
+        Me.lblMonth.Size = New System.Drawing.Size(178, 39)
+        Me.lblMonth.TabIndex = 2
+        Me.lblMonth.Text = "December"
+        Me.lblMonth.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblNumber
+        '
+        Me.lblNumber.AutoSize = True
+        Me.lblNumber.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNumber.Location = New System.Drawing.Point(280, 63)
+        Me.lblNumber.Name = "lblNumber"
+        Me.lblNumber.Size = New System.Drawing.Size(104, 73)
+        Me.lblNumber.TabIndex = 3
+        Me.lblNumber.Text = "00"
+        Me.lblNumber.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblYear
+        '
+        Me.lblYear.AutoSize = True
+        Me.lblYear.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblYear.Location = New System.Drawing.Point(271, 136)
+        Me.lblYear.Name = "lblYear"
+        Me.lblYear.Size = New System.Drawing.Size(90, 39)
+        Me.lblYear.TabIndex = 4
+        Me.lblYear.Text = "Year"
+        Me.lblYear.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'timDisplay
+        '
+        '
+        'Calendar
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(442, 185)
+        Me.Controls.Add(Me.lblYear)
+        Me.Controls.Add(Me.lblNumber)
+        Me.Controls.Add(Me.lblMonth)
+        Me.Controls.Add(Me.lblTime)
+        Me.Controls.Add(Me.lblDay)
+        Me.Name = "Calendar"
+        Me.Text = "My Calendar"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblDay As System.Windows.Forms.Label
+    Friend WithEvents lblTime As System.Windows.Forms.Label
+    Friend WithEvents lblMonth As System.Windows.Forms.Label
+    Friend WithEvents lblNumber As System.Windows.Forms.Label
+    Friend WithEvents lblYear As System.Windows.Forms.Label
+    Friend WithEvents timDisplay As System.Windows.Forms.Timer
+
+End Class
---
+++ SAD-EXER1/Calendar.vb
@@ -0,0 +1,39 @@
+﻿Public Class Calendar
+
+    Private Sub timDisplay_Tick(sender As Object, e As EventArgs) Handles timDisplay.Tick
+        Dim month As Integer = Date.Today.Month
+        lblTime.Text = TimeOfDay()
+        lblNumber.Text = Date.Today.Day
+        lblDay.Text = Date.Today.DayOfWeek.ToString
+        lblYear.Text = Date.Today.Year
+        If month = 1 Then
+            lblMonth.Text = "January"
+        ElseIf month = 2 Then
+            lblMonth.Text = "February"
+        ElseIf month = 3 Then
+            lblMonth.Text = "March"
+        ElseIf month = 4 Then
+            lblMonth.Text = "April"
+        ElseIf month = 5 Then
+            lblMonth.Text = "May"
+        ElseIf month = 6 Then
+            lblMonth.Text = "June"
+        ElseIf month = 7 Then
+            lblMonth.Text = "July"
+        ElseIf month = 8 Then
+            lblMonth.Text = "August"
+        ElseIf month = 9 Then
+            lblMonth.Text = "September"
+        ElseIf month = 10 Then
+            lblMonth.Text = "October"
+        ElseIf month = 11 Then
+            lblMonth.Text = "November"
+        ElseIf month = 12 Then
+            lblMonth.Text = "December"
+        End If
+    End Sub
+
+    Private Sub Calendar_Load(sender As Object, e As EventArgs) Handles Me.Load
+        timDisplay.Start()
+    End Sub
+End Class
